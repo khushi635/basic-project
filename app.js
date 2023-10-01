1341930a56dd314c0eee0685e0b28177fa367a66
khushbu kumari <khushbulike7@gmail.com>
2023-10-01 17:46:06 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+let icon = document.querySelector(".icon");
+icon.addEventListener("click", () => {
+    let container = document.querySelector(".container");
+    container.setAttribute("class", "change")
+})
